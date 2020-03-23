ef11644381989821baa436c8646454c5b1241f0e
hadeed00 <62308478+hadeed00@users.noreply.github.com>
2020-03-23 11:13:47 +0000

    commit on 'master'

---
+++ firebase-functions/public/invig.js
@@ -0,0 +1,31 @@
+
+const userid = document.getElementById('idx')
+const report = document.getElementById('textreport')
+const form = document.getElementById('form')
+const errorElement = document.getElementById("error")
+
+
+
+form.addEventListener('submit', (e) => {
+
+    errorElement.innerText = "XXX"
+
+    let messages = []
+
+    if(userid.value.length != 8 ){
+        messages.push("UserID must have 8 characters")
+    }
+    
+    if(userid.value ==="" || userid.value == null){
+        messages.push("UserID is required")
+    }
+
+    if(report.value.length < 100) {
+        messages.push("Report must have atleast 100 characters.")
+    }
+
+    if (messages.length > 0){
+        e.preventDefault()
+        errorElement.innerText = messages.join(", ")
+    }
+})
